770def93cd2d2260556e1210230a87262a4e8ad8
Logon Aluno <logonrmlocal@fiap.com.br>
2019-04-03 20:45:17 -0300

    Adiciona Projeto

--- Exercicio/src/br/com/fiap/ws/dao/VendaDAO.java
+++
@@ -1,7 +0,0 @@
-package br.com.fiap.ws.dao;
-
-import br.com.fiap.ws.entity.Venda;
-
-public interface VendaDAO extends GenericDAO<Venda, Integer>{
-
-}
--- Exercicio/src/br/com/fiap/ws/resource/VendaResource.java
+++
@@ -1,5 +0,0 @@
-package br.com.fiap.ws.resource;
-
-public class VendaResource {
-
-}
